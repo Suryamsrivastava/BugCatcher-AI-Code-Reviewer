b38f92dfad303c7c182c55cebf7f557749e45428
Suryamsrivastava <suryamsrivastav99@gmail.com>
2025-04-08 03:07:55 +0530

    some changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear()} Aman Singh. All Rights Reserved.</p>
+            <p>© {new Date().getFullYear()} Suryam Srivastava. All Rights Reserved.</p>
         </footer>
     );
 }
