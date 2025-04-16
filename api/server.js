2ad012cbdb77570a33ac88150c4748bcb9cf6724
Suryamsrivastava <suryamsrivastav99@gmail.com>
2025-04-16 20:16:27 +0530

    add port

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+// const port = process.env.PORT || 3000;
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
